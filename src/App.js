d834d2216de5616b989ed6a535eaa6a85befd089
salman g <salman444g@gamil.com>
2022-05-26 11:20:05 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Table from './Table';
 import Form from './Form';
+import {useState} from 'react';
+
 function App(){
+
+  const [getDetails,setDetails]=useState({
+    fullName:"akash",
+    age:22
+  })
+
+  const setDetailsHandler=(inputName,inputAge)=>{
+        setDetails({
+          ...getDetails,
+          fullName:inputName,
+          age:inputAge
+        })
+  }
+
+
    return (<div>
-     <Table/>
-     <Form/>
+     <Table fullName={getDetails.fullName} age={getDetails.age}/>
+     <Form callDetails={(input,age)=>setDetailsHandler(input,age)}/>
    </div>)
 }
 
--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,33 @@
 
-function Table(){
-    return(<div>Table</div>)
+import {useState} from 'react';
+import './Table.css'
+function Table(props){
+    // const[getTable,setTable]=useState({
+    //     fullName:"salman",
+    //     age:22
+    // })
+    // // let fullName = "salman";
+    // // let age = 22;
+    // setTimeout(()=>{
+    //     setTable({
+    //         ...getTable,
+    //         fullName:"akash",
+    //         age:44
+    //     })
+    //       alert("setime");
+    // },3000)
+    return(<div>
+        <table className='row'>
+            <tr>
+                <th>FullName</th>
+                <th>Age</th>
+            </tr>
+            <tr>
+                <td>{props.fullName}</td>
+                <td>{props.age}</td>
+            </tr>
+        </table>
+    </div>)
 }
 
 export default Table;
